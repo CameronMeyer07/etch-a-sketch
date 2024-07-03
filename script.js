97e2f9fe53f7ae9090cf9381b1f4879af09089c5
Mohammed Althaf <mohammedalthaf549@gmail.com>
2024-07-03 16:07:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,41 +3,28 @@ const input = document.querySelector("input");
 const create = document.querySelector(".create");
 const clear = document.querySelector(".clear");
 const row = document.querySelector(".row"); 
-
 create.addEventListener("click", e => {
-
     const myInput = document.querySelector("#value").value;
-
     for (let i = 0; i < myInput; i++){
         let column = document.createElement("div");
         column.classList.add("column");
-    
         for (let j = 1; j <= myInput; j++){
             let row = document.createElement("div");
             row.classList.add("row");
             row.style.backgroundColor = '#dddddd';
             row.addEventListener("mouseover", () => {
             row.style.backgroundColor = `rgb(${randomColor()} ${randomColor()} ${randomColor()})`;})
-            column.append(row);
-        } 
+            column.append(row); } 
         container.append(column);
-        
-        input:focus();
-    }
+        input:focus(); }
 });
-
 clear.addEventListener("click", e => {
-    
     const myInput = document.querySelector("#value").value;
-
     for (let i = 0; i < myInput; i++){
         let column = document.querySelector(".column");
         column.classList.add("column");
-        container.removeChild(column);
-    }
+        container.removeChild(column);}
 })
-
 function randomColor(){
     let randomColor = Math.floor(Math.random() * 256);
-    return randomColor;
-};
+    return randomColor;};
